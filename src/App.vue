6296f595fe9ebbf2bc245e1574db74bdf18cf714
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2024-06-05 19:50:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import myHello from './components/myHello.vue'
+</script>
+
+<template>
+  d
+
+  <myHello title="заголовок" :price="7500">
+    <div>slot children</div>
+  </myHello>
+  <myHello title="заголовок 2 " :price="7400" />
+</template>
---
+++ src/components/myHello.vue
@@ -0,0 +1,41 @@
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script> -->
+<script setup>
+import { CustomButton } from '@/components/ui/button'
+
+// import { ref } from 'vue'
+// defo
+// let count = ref(0)
+// function increment() {
+//   count.value++
+// }
+// function updateCount(event) {
+//   count.value = Number(event.target.value)
+// }
+</script>
+<template>
+  <CustomButton>ff</CustomButton>
+  <div class="mb-40">{{ title }}</div>
+  <div>{{ price }}</div>
+  <!-- <div class="body">{{ count }}</div> -->
+  <!-- <button @click="increment">+</button>
+  <input v-model="count" /> -->
+  <slot />
+</template>
+<style scoped>
+.body {
+  background: red;
+}
+</style>
