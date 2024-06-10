dfbecc5bb1774d6eebc4571355aec60ddc51bc10
Sanya Domashnyi ^_^ <megamanok99@gmail.com>
2024-06-10 15:17:14 +0300

    сверстал формочку

--- src/pages/PostsView.vue
+++ src/pages/PostsView.vue
@@ -1,6 +1,6 @@
 <script>
-import { BillForm } from '@/components/ui/billform/index';
-import { BillTable } from '@/components/ui/billtable/index';
+import { BillForm } from '@/components/ui/billform/index'
+import { BillTable } from '@/components/ui/billtable/index'
 import {
   Pagination,
   PaginationEllipsis,
@@ -9,11 +9,11 @@ import {
   PaginationList,
   PaginationListItem,
   PaginationNext,
-  PaginationPrev,
-} from '@/components/ui/pagination';
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex';
+  PaginationPrev
+} from '@/components/ui/pagination'
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 
-import Input from '@/components/ui/input/Input.vue';
+import Input from '@/components/ui/input/Input.vue'
 
 export default {
   components: {
@@ -27,21 +27,21 @@ export default {
     PaginationPrev,
     BillTable,
     BillForm,
-    Input,
+    Input
   },
   data() {
-    return {};
+    return {}
   },
   methods: {
     ...mapMutations({
       setPage: 'post/setPage',
       setSearchQuery: 'post/setSearchQuery',
-      setSelectedSort: 'post/setSelectedSort',
+      setSelectedSort: 'post/setSelectedSort'
     }),
     ...mapActions({
       fetchPosts: 'post/fetchPosts',
-      Created: 'post/saveBill',
-    }),
+      Created: 'post/saveBill'
+    })
   },
   computed: {
     ...mapState({
@@ -52,27 +52,63 @@ export default {
       isPostLoading: (state) => state.post.isPostLoading,
       limit: (state) => state.post.limit,
       totalPages: (state) => state.post.totalPages,
-      sortOptions: (state) => state.post.sortOptions,
+      sortOptions: (state) => state.post.sortOptions
     }),
     ...mapGetters({
       sortedItems: 'post/sortedItems',
-      sortedAndSearchPosts: 'post/sortedAndSearchPosts',
-    }),
+      sortedAndSearchPosts: 'post/sortedAndSearchPosts'
+    })
   },
   mounted() {
-    this.fetchPosts();
-  },
-};
+    this.fetchPosts()
+  }
+}
 </script>
 
 <template>
+  <div class="m-2 bg-white rounded-xl shadow-md overflow-hidden h-max relative">
+    <div class="md:flex">
+      <BillForm @formSubmit="Created" class="w-full" />
+      <div class="saveImage rounded-xl min-w-96 min-h-64 flex flex-col px-5">
+        <div class="text-slate-50 m-1 text-2xl font-bold">
+          Горячая вода: {{ items[items.length - 1]?.hotWater }} м³
+        </div>
+        <div class="text-slate-50 m-1 text-2xl font-bold">
+          Холодная вода: {{ items[items.length - 1]?.coldWater }} м³
+        </div>
+        <div class="text-slate-50 m-1 text-2xl font-bold">
+          Электричество: {{ items[items.length - 1]?.electric }} Кв/ч
+        </div>
+        <div class="text-slate-50 m-1 text-2xl font-bold">
+          Отвод воды:
+          {{
+            items[items.length - 1]?.coldWater -
+            items[items.length - 2]?.coldWater +
+            items[items.length - 1]?.hotWater -
+            items[items.length - 2]?.hotWater
+          }}
+          м³
+        </div>
+
+        <div class="text-slate-50 m-1 text-2xl font-bold">
+          К оплате
+          {{ items[items.length - 1]?.total }}
+          руб.
+        </div>
+
+        <div class="absolute right-0 bottom-0 m-4 text-slate-50 text-1xl font-bold">
+          Показания от {{ items[items.length - 1]?.createdAt?.split('T')[0] }}
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- <my-select
     :model-value="selectedSort"
     :options="sortOptions"
     @update:model-value="setSelectedSort"
   />
   <Input placeholder="Поиск" :model-value="searchQuery" @update:model-value="setSearchQuery" /> -->
-  <BillForm @formSubmit="Created" />
+
   <BillTable :items="items" @remove="remove" :walkToPost="walkToPost" v-if="!isPostLoading" />
 
   <div v-else>загрузка</div>
